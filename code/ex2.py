8542603d27240b40b029ed80ac2af99333c24537
jackzhao-mj <jackzhao.mj@gmail.com>
2015-06-14 23:25:37 -0700

    finish the luhn algorithm

--- code/ex2.py
+++ code/ex2.py
@@ -15,3 +15,17 @@ def sum_digits(n):
 # 		allbutlast, last = split(n)
 # 		return sum_digits(allbutlast) + last
 
+def luhn_sum(n):
+	if n < 10:
+		return n
+	else:
+		allbutlast, last = split(n)
+		return luhn_sum_double(allbutlast) + last
+
+def luhn_sum_double(n):
+	allbutlast, last = split(n)
+	luhn_digit = sum_digits(2 * last)
+	if n < 10:
+		return luhn_digit
+	else:
+		return luhn_sum(allbutlast) + luhn_digit
